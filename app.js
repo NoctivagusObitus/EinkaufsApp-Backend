52add90d6ef8103f934ac5a0adc845873d5d1a59
bastisk <basti.sk@gmx.de>
2015-10-18 15:44:06 +0200

    Added main in package.json

--- app.js
+++ app.js
@@ -27,8 +27,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));

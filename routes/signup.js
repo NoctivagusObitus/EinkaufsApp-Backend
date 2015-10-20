758f175a6fa74e5742970182526977559509d23b
bastisk <basti.sk@gmx.de>
2015-10-20 14:45:02 +0200

    Added app register

--- routes/signup.js
+++ routes/signup.js
@@ -49,4 +49,40 @@ router.post('/', function(req, res) {
     });
 });
 
+router.post('/app', function(req, res) {
+    if (!validateEmail(req.body.email)){
+      req.flash('error', 'Deine E-Mail Adresse scheint nicht korrekt zu sein.');
+      return res.send({status: 'error', message: 'Deine E-Mail Adresse scheint nicht korrekt zu sein.'});
+    }
+    if (req.body.password != req.body.confirm){
+      req.flash('error', 'Passwörter nicht gleich');
+      return res.send({status: 'error', message: 'Passwörter nicht gleich'});
+    }
+    User.findOne( {username: req.body.username}, function (err, user) {
+      if (err) { console.log(err); }
+      if (user) {
+        req.flash('error', 'Benutzername bereits vergeben.');
+        return res.send({status: 'error', message: 'Benutzername bereits vergeben.'});
+      }
+        User.findOne( {email: req.body.email}, function (err, user) {
+          if (err) { console.log(err); }
+          if (user) {
+            req.flash('error', 'E-Mail bereits vergeben.');
+            return res.send({status: 'error', message: 'E-Mail bereits vergeben.'});
+        }
+            var user = new User({
+              username: req.body.username,
+              email: req.body.email,
+              password: req.body.password
+            });
+
+          user.save(function(err) {
+              req.logIn(user, function(err) {
+              res.send({status: 'ok'});
+            });
+          });
+        });
+    });
+});
+
 module.exports = router;

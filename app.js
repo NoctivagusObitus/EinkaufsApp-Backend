e0951a0a16f1b1767ecb0442e0f3a3686f0d2542
bastisk <basti.sk@gmx.de>
2015-10-20 11:34:16 +0200

    Added Allow Origin

--- app.js
+++ app.js
@@ -56,11 +56,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-app.all('/', function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
- });
+});
 app.use('/', routes);
 app.use('/user', users);
 app.use('/article', artikel);

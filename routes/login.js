3bbfe2f058dfa24700b9c4211aeeb9e192c5bf69
swemicha <ichbin7cool7@gmx.de>
2015-10-27 17:30:37 +0100

    Bugfix signup and new layout

--- routes/login.js
+++ routes/login.js
@@ -17,29 +17,11 @@ router.post('/', function(req, res, next) {
     }
     req.logIn(user, function(err) {
       if (err) {
-        req.flash('error', 'Login fehlgeschlagen - PW');
+        req.flash('error', 'Login fehlgeschlagen');
         return next(err);
       }
       req.flash('success', 'Login erfolgreich');
       return res.redirect('/Inside');
-    });
-  })(req, res, next);
-});
-
-router.post('/app', function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
-    if (err) return next(err)
-    if (!user) {
-      req.flash('error', 'Login fehlgeschlagen');
-      return res.send({status: "error", message: 'Login fehlgeschlagen'});
-    }
-    req.logIn(user, function(err) {
-      if (err) {
-        req.flash('error', 'Login fehlgeschlagen - PW');
-        return res.send({status: "error", message: 'Login fehlgeschlagen - PW'});
-      }
-      req.flash('success', 'Login erfolgreich');
-      return res.send({status: "ok"});
     });
   })(req, res, next);
 });
--- routes/signup.js
+++ routes/signup.js
@@ -9,31 +9,54 @@ function validateEmail(email) {
 
 router.get('/', function(req, res) {
   res.render('signup', {
-    user: req.user
+    user: '',
+    cusername: '',
+    cmail: '',
+    cpassword: '',
+    cpasswordconfirm: ''
   });
 });
 
 router.post('/', function(req, res) {
     if (!validateEmail(req.body.email)){
-      req.flash('error', 'Deine E-Mail Adresse scheint nicht korrekt zu sein.');
-      return res.redirect('/signup')
+      req.flash('error', 'Deine E-Mail Adresse scheint nicht korrekt zu sein  ');
+      //return res.redirect('/signup')
+      var emailx = '';
+    }else{
+      var emailx = req.body.email;   
     }
     if (req.body.password != req.body.confirm){
-      req.flash('error', 'Passwörter nicht gleich');
-      return res.redirect('/signup')
+      req.flash('error', 'Passwörter nicht gleich  ');
+      var pw = '';
+      var cpw = '';
+    }else{
+      var pw = req.body.password;   
+      var cpw = req.body.confirm;
     }
     User.findOne( {username: req.body.username}, function (err, user) {
       if (err) { console.log(err); }
       if (user) {
-        req.flash('error', 'Benutzername bereits vergeben.');
-        return res.redirect('/signup')
+        req.flash('error', 'Benutzername bereits vergeben  ');
+        var userx = '';
+      }else{
+        var userx = req.body.username;   
       }
         User.findOne( {email: req.body.email}, function (err, user) {
           if (err) { console.log(err); }
           if (user) {
-            req.flash('error', 'E-Mail bereits vergeben.');
-            return res.redirect('/signup')
+            req.flash('error', 'E-Mail bereits vergeben  ');
+            var email = '';
+        }else{
+            var email = req.body.email;   
         }
+            if (email || pw || cpw || user == ''){
+              return res.render('signup', {
+                  cusername: userx,
+                  cmail: emailx,
+                  cpassword: pw,
+                  cpasswordconfirm: cpw
+              });        
+            }
             var user = new User({
               username: req.body.username,
               email: req.body.email,
@@ -43,42 +66,6 @@ router.post('/', function(req, res) {
           user.save(function(err) {
               req.logIn(user, function(err) {
               res.redirect('/Inside');
-            });
-          });
-        });
-    });
-});
-
-router.post('/app', function(req, res) {
-    if (!validateEmail(req.body.email)){
-      req.flash('error', 'Deine E-Mail Adresse scheint nicht korrekt zu sein.');
-      return res.send({status: 'error', message: 'Deine E-Mail Adresse scheint nicht korrekt zu sein.'});
-    }
-    if (req.body.password != req.body.confirm){
-      req.flash('error', 'Passwörter nicht gleich');
-      return res.send({status: 'error', message: 'Passwörter nicht gleich'});
-    }
-    User.findOne( {username: req.body.username}, function (err, user) {
-      if (err) { console.log(err); }
-      if (user) {
-        req.flash('error', 'Benutzername bereits vergeben.');
-        return res.send({status: 'error', message: 'Benutzername bereits vergeben.'});
-      }
-        User.findOne( {email: req.body.email}, function (err, user) {
-          if (err) { console.log(err); }
-          if (user) {
-            req.flash('error', 'E-Mail bereits vergeben.');
-            return res.send({status: 'error', message: 'E-Mail bereits vergeben.'});
-        }
-            var user = new User({
-              username: req.body.username,
-              email: req.body.email,
-              password: req.body.password
-            });
-
-          user.save(function(err) {
-              req.logIn(user, function(err) {
-              res.send({status: 'ok'});
             });
           });
         });

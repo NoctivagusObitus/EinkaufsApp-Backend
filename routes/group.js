9023c391853633bc78dd464c769258afd9eb3637
bastisk <basti.sk@gmx.de>
2015-11-03 16:46:36 +0100

    Added BL for Groups

--- routes/group.js
+++ routes/group.js
@@ -15,4 +15,42 @@ router.get('/:id', function (req, res, next){
     });
 });
 
+router.get('/creator/:id', function (req, res, next){
+    Group.find({creator: req.params.id}, function(err, groups){
+        res.send(groups);
+    });
+});
+
+router.post('/edit/:id', auth, function(req, res, next){
+    Group.findbyId(req.params.id, function (err, group){
+        if (err) {
+            console.log(err);
+            res.send(err);
+        }
+        group.name = req.body.name;
+        group.users = req.body.users;  
+        group.save(function (err){
+            if (err) {
+                console.log(err);
+                res.send(err);
+            }
+        });
+    });
+});
+
+router.post('/add', auth, function(req, res, next){
+    var new_group = new Group({name: req.body.name, users: req.body.users, creator: req.body.creator});
+    new_group.save(function(err){
+        console.log(err);
+    });
+    res.send("k");
+});
+
+router.post('/delete/:id', auth, function(req, res, next){
+    Group.remove({_id: req.params.id }, function(err){
+        console.log(err);
+        res.send("k");
+    });
+});
+
 module.exports = router;

b6891600cf8a9cfc786b2995b6585b252d05c3d8
Sebastian Kiepsch <basti.sk@gmx.de>
2015-12-06 17:22:44 +0100

    bla

--- routes/purchase.js
+++ routes/purchase.js
@@ -9,105 +9,145 @@ var Group = require('../models/group');
 
 var auth = require('../conf/auth.js');
 
-router.get('/', auth, function (req, res, next) {
-    Purchase.find(function (err, purchase) {
-        res.send(purchase);
-    });
+router.get('/', auth, function(req, res, next) {
+  Purchase.find(function(err, purchase) {
+    res.send(purchase);
+  });
 });
 
-router.get('/:id', auth, function (req, res, next) {
-    Purchase.findbyid(req.params.id, function (err, purchase) {
-        res.send(purchase);
-    });
+router.get('/:id', auth, function(req, res, next) {
+  Purchase.findbyid(req.params.id, function(err, purchase) {
+    res.send(purchase);
+  });
 });
 
-router.get('/owner/:owner', auth, function (req, res, next) {
-    Purchase.find({
-        owner_id: req.params.owner
-    }, function (err, purchases) {
-        res.send(purchases);
-    })
+router.get('/owner/:owner', auth, function(req, res, next) {
+  Purchase.find({
+    owner_id: req.params.owner
+  }, function(err, purchases) {
+    res.send(purchases);
+  })
 });
 
-router.get('/benefitial/:benefitial', auth, function (req, res, next) {
-    Purchase.find({
-        'cart.benefitial_id': req.params.benefitial
-    }, function (err, purchases) {
-        res.send(purchases);
-    })
+router.get('/benefitial/:benefitial', auth, function(req, res, next) {
+  Purchase.find({
+    'cart.benefitial_id': req.params.benefitial
+  }, function(err, purchases) {
+    res.send(purchases);
+  })
 });
 
-router.get('/rich/:id', auth, function (req, res, next) {
-    var richoutput = {};
-    Purchase.findbyId(req.params.id, function (err, purchase) {
-        User.findbyId(purchase.owner_id, function (err, user) {
-            richoutput.owner = user.name;
-            Store.findbyId(purchase.store_id, function (err, store) {
-                richoutput.store = store.name;
-                richoutput.cart = [];
-                for (var i = 0; i < purchase.cart.length; i++) {
-                    User.findbyId(purchase.cart[i].benefitial_id, function (err, user) {
-                        richoutput.cart[i].benefitial = user.name;
-                        Article_store.findbyId(purchase.cart[i].article_store_id, function (err, articlestore) {
-                            richoutput.cart[i].price = articlestore.costs.price;
-                            Article.findbyId(articlestore.article_id, function (err, article) {
-                                richoutput.cart[i].article = article.name;
-                                res.send(richoutput);
-                            })
-                        });
-                    });
-                }
+router.get('/rich/:id', auth, function(req, res, next) {
+  var richoutput = {};
+  Purchase.findbyId(req.params.id, function(err, purchase) {
+    User.findbyId(purchase.owner_id, function(err, user) {
+      richoutput.owner = user.name;
+      Store.findbyId(purchase.store_id, function(err, store) {
+        richoutput.store = store.name;
+        richoutput.cart = [];
+        for (var i = 0; i < purchase.cart.length; i++) {
+          User.findbyId(purchase.cart[i].benefitial_id, function(err, user) {
+            richoutput.cart[i].benefitial = user.name;
+            Article_store.findbyId(purchase.cart[i].article_store_id, function(err, articlestore) {
+              richoutput.cart[i].price = articlestore.costs.price;
+              Article.findbyId(articlestore.article_id, function(err, article) {
+                richoutput.cart[i].article = article.name;
+                res.send(richoutput);
+              })
             });
-        });
+          });
+        }
+      });
     });
+  });
 });
 
-router.post('/add', auth, function (req, res, next) {
-    var purchase = new Purchase({
-        date: req.body.date,
-        buyer_user: req.body.buyer_user,
-        benefitial_user: req.body.benefitial_user,
-        benefitial_group: req.body.benefitial_group,
-        purchased_articles: req.body.purchased_articles
-    });
-    Purchase.save(function (err) {
-        if (err) res.send({
-            status: "error",
-            message: err
+router.post('/add', auth, function(req, res, next) {
+  var cart = req.body.cart;
+
+  for (var i = 0; i < cart.length; i++) {
+    var article = cart[i].article_costs.article;
+    var cart = [];
+
+    Article.find({ean: article.ean}, function(err, article) {
+      var articleid;
+      var articlecostsid;
+
+      if (err) {
+        var newarticle = new Article(article);
+        newarticle.save(function(err, art) {
+          if (err) console.log(err);
+          else articleid = art._id;
         });
-        else res.send({
-            status: "ok"
+      } else {
+        articleid = article._id;
+      }
+        ArticleCost.find({store_id: cart[i].article_costs.store_id,article_id: articleid}, function(err, entity) {
+          if(err){
+            var newentity = new ArticleCost({
+              store_id: cart[i].article_costs.store_id,
+              article_id: article._id,
+              costs: cart[i].article_costs.costs,
+              offer: cart[i].article_costs.offer
+            });
+            newentity.save(function(err, ent) {
+              if (err) console.log(err);
+              else articelcostsid = ent._id;
+            });
+          } else articlecostsid = entity._id;
+          cart.push({article_store_id: articlecostsid, amount: cart[i].amount, benefitial_id: '0' });
         });
     });
+  }
+ console.log(cart);
+  var purchase = new Purchase({store_id: req.body.store_id, owner_id: req.body.owner_id, date: req.body.date, cart: cart })
+ console.log(purchase);
+/*
+  var purchase = new Purchase({
+    date: req.body.date,
+    buyer_user: req.body.buyer_user,
+    benefitial_user: req.body.benefitial_user,
+    benefitial_group: req.body.benefitial_group,
+    purchased_articles: req.body.purchased_articles
+  });*/
+  purchase.save(function(err) {
+    if (err) res.send({
+      status: "error",
+      message: err
+    });
+    else res.send({
+      status: "ok"
+    });
+  });
 });
 
-router.post('/edit/:id', auth, function (req, res, next) {
-    Purchase.findbyId(req.params.id, function (err, purchase) {
-        if (err) {
-            console.log(err);
-            res.send(err);
-        } else res.send("k");
-        purchase.date = req.body.date;
-        purchase.buyer_user = req.article.buyer_user;
-        purchase.benefitial_user = req.body.benefitial_user;
-        purchase.benefitial_group = req.body.benefitial_group;
-        purchase.purchased_articles = req.body.purchased_articles;
-        purchase.save(function (err) {
-            if (err) {
-                console.log(err);
-                res.send(err);
-            } else res.send("k");
-        });
-    });
-});
-
-router.post('/delete/:id', auth, function (req, res, next) {
-    Purchase.remove({
-        _id: req.params.id
-    }, function (err) {
+router.post('/edit/:id', auth, function(req, res, next) {
+  Purchase.findbyId(req.params.id, function(err, purchase) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else res.send("k");
+    purchase.date = req.body.date;
+    purchase.buyer_user = req.article.buyer_user;
+    purchase.benefitial_user = req.body.benefitial_user;
+    purchase.benefitial_group = req.body.benefitial_group;
+    purchase.purchased_articles = req.body.purchased_articles;
+    purchase.save(function(err) {
+      if (err) {
         console.log(err);
-        res.send("k");
+        res.send(err);
+      } else res.send("k");
     });
+  });
+});
+
+router.post('/delete/:id', auth, function(req, res, next) {
+  Purchase.remove({
+    _id: req.params.id
+  }, function(err) {
+    console.log(err);
+    res.send("k");
+  });
 });
 
 module.exports = router;

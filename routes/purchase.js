ebf96a75696aff3f7981547504d53bf74ebf08f8
Sebastian Kiepsch <basti.sk@gmx.de>
2015-12-09 18:35:12 +0100

    sdfsdf

--- routes/purchase.js
+++ routes/purchase.js
@@ -65,9 +65,9 @@ router.get('/rich/:id', auth, function(req, res, next) {
 
 router.post('/add', auth, function(req, res, next) {
   console.log(req.body);
-  var cart = req.body["cart"];
-  console.log = req.body["cart"][0]["amount"];
-  console.log(cart);
+  console.log(req.body.cart[0]['amount']);
+  console.log(req.body.cart[0].amount);
+  var cart = req.body.cart;
 
   for (var i = 0; i < cart.length; i++) {
     var article = cart[i].article_costs.article;
